1a640000071eac37e630a41963f76a303d774cf4
kubabartecki <169511@stud.prz.edu.pl>
2024-07-07 14:16:07 +0200

    add put and delete methods

--- backend/task/src/main/java/com/bartheme/task/controller/TaskController.java
+++ backend/task/src/main/java/com/bartheme/task/controller/TaskController.java
@@ -2,6 +2,9 @@ package com.bartheme.task.controller;
 
 import com.bartheme.task.dto.TaskCreateDto;
 import com.bartheme.task.dto.TaskDto;
+import com.bartheme.task.dto.TaskUpdateDto;
+import com.bartheme.task.exception.ResourceNotFoundException;
+import com.bartheme.task.exception.UnprocessableContentException;
 import com.bartheme.task.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,16 +17,24 @@ public class TaskController {
     private final TaskService taskService;
 
     @GetMapping("{id}")
-    public ResponseEntity<TaskDto> getTaskById(@PathVariable Integer id) {
+    public ResponseEntity<TaskDto> getTaskById(@PathVariable Integer id) throws ResourceNotFoundException {
         TaskDto taskDto = taskService.getTaskById(id);
-        if (taskDto == null) {
-            return ResponseEntity.notFound().build();
-        }
         return ResponseEntity.ok(taskDto);
     }
 
     @PostMapping
     public ResponseEntity<TaskDto> createTask(@RequestBody TaskCreateDto taskCreateDto) {
         return ResponseEntity.ok(taskService.createTask(taskCreateDto));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<TaskDto> updateTask(@PathVariable Integer id, @RequestBody TaskUpdateDto taskUpdateDto)
+            throws ResourceNotFoundException, UnprocessableContentException {
+        return ResponseEntity.ok(taskService.updateTask(id, taskUpdateDto));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteTask(@PathVariable Integer id) throws ResourceNotFoundException {
+        return ResponseEntity.ok(taskService.deleteTask(id));
     }
 }
--- backend/task/src/main/java/com/bartheme/task/service/TaskService.java
+++ backend/task/src/main/java/com/bartheme/task/service/TaskService.java
@@ -1,6 +1,9 @@
 package com.bartheme.task.service;
 
 import com.bartheme.task.dto.TaskCreateDto;
+import com.bartheme.task.dto.TaskUpdateDto;
+import com.bartheme.task.exception.ResourceNotFoundException;
+import com.bartheme.task.exception.UnprocessableContentException;
 import com.bartheme.task.model.Task;
 import com.bartheme.task.dto.TaskDto;
 import com.bartheme.task.model.TaskStatus;
@@ -9,19 +12,50 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class TaskService {
     private final TaskRepository taskRepository;
 
-    public TaskDto getTaskById(Integer id) {
-        Optional<Task> task =  taskRepository.findById(id);
-        return task.map(TaskDto::new).orElse(null);
+    public TaskDto getTaskById(Integer id) throws ResourceNotFoundException {
+        Task task =  taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task not found for id: " + id));
+        return new TaskDto(task);
     }
 
     public TaskDto createTask(TaskCreateDto taskCreateDto) {
+        Task task = createTaskFromTaskCreateDto(taskCreateDto);
+        Task savedTask = taskRepository.save(task);
+        return new TaskDto(savedTask);
+    }
+
+    public TaskDto updateTask(Integer id, TaskUpdateDto taskUpdateDto)
+            throws ResourceNotFoundException, UnprocessableContentException {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task not found for id: " + id));
+        task.setTitle(taskUpdateDto.getTitle());
+        task.setDescription(taskUpdateDto.getDescription());
+        task.setCategory(taskUpdateDto.isUrgent(), taskUpdateDto.isImportant());
+        try {
+            task.setStatus(TaskStatus.valueOf(taskUpdateDto.getStatus()));
+        } catch (IllegalArgumentException e) {
+            throw new UnprocessableContentException("Not valid status enum value");
+        }
+        task.setDueDate(LocalDateTime.parse(taskUpdateDto.getDueDate()));
+        taskRepository.save(task);
+        return new TaskDto(task);
+    }
+
+
+    public String deleteTask(Integer id) throws ResourceNotFoundException {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task not found for id: " + id));
+        taskRepository.delete(task);
+        return "Task deleted";
+    }
+
+    private Task createTaskFromTaskCreateDto(TaskCreateDto taskCreateDto) {
         Task task = new Task();
         task.setTitle(taskCreateDto.getTitle());
         task.setDescription(taskCreateDto.getDescription());
@@ -29,7 +63,6 @@ public class TaskService {
         task.setCategory(taskCreateDto.isUrgent(), taskCreateDto.isImportant());
         task.setCreatedAtDate(LocalDateTime.now());
         task.setDueDate(LocalDateTime.parse(taskCreateDto.getDueDate()));
-        Task savedTask = taskRepository.save(task);
-        return new TaskDto(savedTask);
+        return task;
     }
 }
