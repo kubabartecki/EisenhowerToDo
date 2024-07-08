80c97779f758f910046115ed274a2a9bcdefa82a
kubabartecki <169511@stud.prz.edu.pl>
2024-07-08 13:20:00 +0200

    enable theming and routing in `App.tsx`, also added `NoMatch` page

--- frontend/todo-list/src/App.tsx
+++ frontend/todo-list/src/App.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
-const App: React.FC = () =>{
+import NavigationBar from './components/navbar/NavigationBar';
+import HomePage from './pages/home/HomePage';
+import AddTask from "./pages/add-task/AddTask";
+import TaskMatrix from "./pages/matrix/TaskMatrix";
+
+import theme from './theme/theme';
+import './App.css';
+import "./theme/variables.css";
+import "./theme/global.scss";
+
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        Todo
-      </header>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <NavigationBar></NavigationBar>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="add" element={<AddTask />} />
+          <Route path="matrix" element={<TaskMatrix />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
     </div>
   );
 }
