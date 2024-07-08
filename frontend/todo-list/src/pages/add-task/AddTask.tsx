361e76d1493cc75016e2a9b3cbe321dfa380dc2c
kubabartecki <169511@stud.prz.edu.pl>
2024-07-08 13:22:57 +0200

    empty add page

---
+++ frontend/todo-list/src/pages/add-task/AddTask.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+
+import './AddTask.scss';
+
+
+const AddTask: React.FC = () => {
+  return (
+    <Box className="" sx={{ width: '100%' }}>
+      AddTask
+    </Box>
+  );
+}
+
+export default AddTask;
